38622be3d5a5beba204891207c45df47920295c4
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-01-14 14:10:19 +0800

    [xiaojun] Use "executor" for Heroku Orb

--- dso/views.py
+++ dso/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('Home page for Regional IT Team.')
+  return HttpResponse('Home page for Regional IT Team. -- Xiaojun')
