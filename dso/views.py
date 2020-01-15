444ea50a4f54ea07365039db9dde5d821c02950c
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-01-15 11:37:42 +0800

    [xiaojun] Add QA stage

--- dso/views.py
+++ dso/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('Home page for Regional IT Team. -- Xiaojun 2020-01-15 11:22:57')
+  return HttpResponse('Home page for Regional IT Team. -- Xiaojun 2020-01-15 11:37:33')
