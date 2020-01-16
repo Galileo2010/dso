bd2741a5e2c753548cba670be9a1527c6505948a
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-01-16 15:09:00 +0800

    [xiaojun] Hawkeyesec asked to update

--- dso/views.py
+++ dso/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('Home page for Regional IT Team. -- Xiaojun 2020-01-15 14:34:54')
+  return HttpResponse('Home page for Regional IT Team. -- Xiaojun 2020-01-16 15:08:49')
