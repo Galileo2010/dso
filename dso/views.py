549fced56ee3905c37a3f0f1f28b6e78a066f6a4
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-01-18 02:18:47 +0800

    [xiaojun] Use circleci/gcp-cli@1.8.3 to deploy to gcp

--- dso/views.py
+++ dso/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('Home page for Regional IT Team. -- Xiaojun 2020-01-18 00:33:13')
+  return HttpResponse('Home page for Regional IT Team. -- Xiaojun  2020-01-18 02:16:44')
